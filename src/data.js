2240df4e10865fb04897755cc22a7ec191027e36
nimrahSohail <102296918+nimrahSohail@users.noreply.github.com>
2022-10-08 23:52:38 +0500

    Second slider model

---
+++ src/data.js
@@ -0,0 +1,44 @@
+import img1 from  './images/1.jpg';
+import img2 from  './images/2.jpg';
+import img3 from  './images/3.jpg';
+import img4 from  './images/4.jpg';
+import img5 from  './images/5.jpg';
+import img6 from  './images/6.jpg';
+
+export const dataDigitalBestSeller = [
+    {
+        id: 1,
+        title: 'Insta Image 1',
+        linkimg: img1
+    },
+
+    {
+        id: 2,
+        title: 'Insta Image 2',
+        linkimg: img2
+    },
+
+    {
+        id: 3,
+        title: 'Insta Image 3',
+        linkimg: img3
+    },
+
+    {
+        id: 4,
+        title: 'Insta Image 4',
+        linkimg: img4
+    },
+
+    {
+        id: 5,
+        title: 'Insta Image 5',
+        linkimg: img5
+    },
+
+    {
+        id: 6,
+        title: 'Insta Image 6',
+        linkimg: img6
+    }
+]
